8a07b3575103df5d5dfb510b15265576b056260f
totolemarto <tommy.prats@etu.univ-grenoble-alpes.fr>
2024-12-04 11:50:59 +0100

    change weight by width ... more sense xD

--- jour_4/part2.py
+++ jour_4/part2.py
@@ -11,9 +11,9 @@ for line in x.split("\n"):
     mat.append( list(line) )
 number = 0
 height = len(mat) - 1
-weight = len(mat[0])
+width = len(mat[0])
 for i in range(1, height - 1):
-    for j in range(1, weight - 1):
+    for j in range(1, width - 1):
         count = 0
         if (mat[i][j] != "A"):
             continue
