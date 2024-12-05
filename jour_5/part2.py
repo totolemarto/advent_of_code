315dcdedbea535489f92b40cc39a1ee1dfa9aeea
totolemarto <tommy.prats@etu.univ-grenoble-alpes.fr>
2024-12-05 14:02:15 +0100

    better version but this recursive function seems hard to understand ... Don't care it's not important code

--- jour_5/part2.py
+++ jour_5/part2.py
@@ -9,34 +9,25 @@ if len(argv) != 2 :
 with open(argv[1],"r") as f:
     x = f.read()
 
-# 5524 > too low
-
 def construit_ligne(good_order, line2, line, good_len, current,pos_line = 0, finish = 0):
     global i
-
-    print(f" on m'appelle sur {i=} et {line2=} et {good_len=} et {pos_line=}")
     if i == good_len + 1: 
         return line2
-    while good_order[current ] == []:
+    while good_order[current] == []:
         if current in line2: 
             return line2
-            input("je suis dans la fun")
         line2.append(current)
         i += 1
         if i == good_len + 1 or finish:
             return line2
         pos_line += 1
         current = line[pos_line]
-    mini = 0
     to_do = []
     for elem in good_order[current]:
         to_do.append([elem, good_order[elem]])
-
     to_do = sorted(to_do, key=lambda lenght: len(lenght[1]))
-    print(f"{to_do=}")
     new_to_do = []
     j = 0
-    tmpi = i
     while  j < len(to_do) :
         new_to_do_tmp = []
         x = j + 1
@@ -44,12 +35,9 @@ def construit_ligne(good_order, line2, line, good_len, current,pos_line = 0, fin
         while x < len(to_do) and len(to_do[j][1]) == len(to_do[x][1]):
             new_to_do_tmp.append(to_do[x])
             x += 1
-        new_to_do_tmp = sorted( new_to_do_tmp, key = lambda order : line.index(order[0]))
-        print(new_to_do_tmp)
-        new_to_do+= new_to_do_tmp # preserve original order
+        new_to_do += sorted( new_to_do_tmp, key = lambda order : line.index(order[0])) # preserve original order of element in line
         j = x
     to_do = new_to_do
-    print(f"{to_do=}")
     for elem in to_do:
         if elem in line2:continue
         line2=  construit_ligne(good_order, line2, line, good_len, elem[0],pos_line + 1, finish = 1)
@@ -59,8 +47,6 @@ def construit_ligne(good_order, line2, line, good_len, current,pos_line = 0, fin
     if current not in line2:
         line2.append(current)
         i += 1
-    tmpi += 1
-    #breakpoint()
     if i == good_len + 1 or finish: 
         return line2
     while line[pos_line] in line2:
@@ -68,12 +54,33 @@ def construit_ligne(good_order, line2, line, good_len, current,pos_line = 0, fin
     return construit_ligne(good_order, line2, line, good_len, line[pos_line], pos_line)
 
 
+def is_good(union, line,test):
+    flag2=0
+    i = 0
+    elem = line[0]
+    good_order = {}
+    while 1:
+        intersect = []
+        if elem in union.keys():
+            if not test:
+                intersect = [ x for x in union[elem] if x in line[i+1:]] 
+                if intersect != [] : 
+                    flag2=1
+            else:
+                intersect = [ x for x in union[elem] if x in line] 
+        good_order[elem] = intersect
+        i += 1
+        if i == len(line):
+            break
+        elem = line[i]
+    return flag2, good_order
+
 union = {}
 mat = []
 flag = 1
 sume = 0
 for line in x.split("\n"):
-    if flag and not "|" in line : 
+    if flag and not "|" in line : # get the empty line between rules and pages 
         flag = 0
         continue
     if flag:
@@ -83,72 +90,12 @@ for line in x.split("\n"):
         else:
             union[y].append(x)
     else:
-        flag2=0
         line = line.split(",")
-        if line[0] == '':
-            break
-        i = 0
-        elem = line[0]
-        good_order = {}
-        while 1:
-            #            print(i, elem)
-            if elem in union.keys():
-                intersect = [ x for x in union[elem] if x in line[i+1:]] 
-                if intersect != [] :
-                    flag2=1
-                print(elem, intersect)
-            else:
-                intersect = []
-            good_order[elem] = intersect
-            if intersect != []:
-                print( elem,intersect)
-                flag2=1
-                print("ça pete")
-            i += 1
-            if i == len(line):
-                break
-            elem = line[i]
+        flag2, _ = is_good(union, line, 0)
         if not flag2:continue 
+        _, good_order = is_good( union, line, 1)
+        good_len = len(line)//2 
         i = 0
-        elem = line[0]
-        good_order = {}
-        while 1:
-            if elem in union.keys():
-                intersect = [ x for x in union[elem] if x in line] 
-            else:
-                intersect = []
-            good_order[elem] = intersect
-            i += 1
-            if i == len(line):
-                break
-            elem = line[i]
-        print(good_order)
-        good_len = len(line)//2 if len(line) % 2 == 1 else len(line) // 2 - 1
-        line2 = []
-        i = 0
-        print("valeuur de line :",line)
-        line2 = construit_ligne(good_order, line2, line, good_len, line[0])    
-        i = 0
-        elem = line2[0]
-        good_order = {}
-        while 1:
-            #            print(i, elem)
-            if elem in union.keys():
-                intersect = [ x for x in union[elem] if x in line2[i + 1:]] 
-            else:
-                intersect = []
-            good_order[elem] = intersect
-            if intersect != []:
-                print( elem,intersect)
-                flag2=1
-                print(line)
-                print(line2)
-                input("pourquoi ça a valide") 
-            i += 1
-            if i == len(line2):
-                break
-            elem = line2[i]
-        print(line2, good_len)
-        print("je rajoute donc", line2[good_len])
+        line2 = construit_ligne(good_order, [] , line, good_len, line[0])    
         sume += int(line2[good_len])
 print(sume)
