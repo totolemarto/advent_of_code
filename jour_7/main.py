bd23e0b124f80b013517f387f08d3fe6b9d22aaf
totolemarto <tommy.prats@etu.univ-grenoble-alpes.fr>
2024-12-07 10:51:51 +0100

    first part

---
+++ jour_7/main.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+from sys import argv
+
+def compute(goal, liste, index, lenght, current):
+    my_print(f"{goal=}, {current=}")
+    if index == lenght:
+        return current ==  goal
+    return compute(goal, liste, index +1, lenght, current + int(liste[index])) or compute(goal, liste, index +1, lenght, current * int(liste[index]))
+
+
+def my_print(*args, **kwargs):
+    global silent
+    if silent: print(*args, **kwargs)
+if len(argv) != 3  :
+    print(f"usage : {argv[0]} <fichier> <debug>")
+    exit(1)
+with open(argv[1],"r") as f:
+    x = f.read().strip()
+silent = int(argv[2]) 
+tot = 0
+for line in x.split("\n"):
+    goal = int(line.split(":")[0])
+    liste = str(line.split(":")[1])
+    liste= liste.split()
+    taille = len(liste)
+    if compute(goal, liste, 0, taille, 0):
+        tot += goal
+print(tot, "ici")
