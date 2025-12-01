0aff96ce28c169d9ed671a01311f1355fc8aa9e2
Tommy Prats <tommy.prats@etu.univ-grenoble-alpes.fr>
2025-12-01 18:35:24 +0100

    second star is good, I begin in rust, that's why my code is ugly

--- 2025/jour_1/src/main.rs
+++ 2025/jour_1/src/main.rs
@@ -3,38 +3,49 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() != 3{
+        println!("usage <file to read> < step for stars>");
+        std::process::exit(1);
+    }
+
     let file_name = &args[1];
-    let contenu = fs::read_to_string(file_name)
-        .expect("Quelque chose s'est mal passé lors de la lecture du fichier");
+    let mode = &args[2];
+    let content = fs::read_to_string(file_name)
+        .expect("Error while reading file");
+
     let mut current : i32 = 50;
     let mut result : i32 = 0;
-    let split_value = contenu.split("\n");
+    let split_value = content.split("\n");
+
     for value in split_value{
         if value.len() < 1{
             break;
         }
-        println!("{value} {current}");
         let numberstr = &value[1..];
         let offset : i32 = numberstr.parse::<i32>().unwrap();
-        if value.starts_with("L"){
+        let mut skip = current == 0;
 
-            current -= offset;
+        current += if value.starts_with("L") {-offset} else {offset};
 
-        } else {
-            current += offset;
-        }
         while current < 0 || current > 99{
             if current > 99 {
+                if mode.eq("2") {
+                    result += current / 100;
+                }
                 current = current % 100;
+                if current == 0{
+                    result -= 1;
+                }
             }
             if current < 0{
+                if mode.eq("2") &&  !skip {
+                    result += 1;
+                }
                 current += 100;
             }
+            skip = false;
         }
-        if current == 0{
-            result += 1;
-        }
+        result += if current == 0 {1} else {0};
     }
-    
     println!("{result} ");
 }
