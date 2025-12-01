cbf989b4ec5d5bbe8681f27d5f38b464ad8ece23
Tommy Prats <tommy.prats@etu.univ-grenoble-alpes.fr>
2025-12-01 23:52:59 +0100

    second star

--- 2015/jour_5/src/main.rs
+++ 2015/jour_5/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::collections::HashMap;
 
 fn init() -> (String, String) {
     let args: Vec<String> = env::args().collect();
@@ -45,12 +46,35 @@ fn analyze_word(line: &str) -> i32 {
     return 0;
 }
 
+fn analyze_word_part_2(line: &str) -> i32 {
+    let mut couple_letter: HashMap<String, usize> = HashMap::new();
+    let mut double = false;
+    let mut good = false;
+    for (i, charac) in line.chars().enumerate() {
+        if i == 0{
+            continue;
+        }
+        double = double || ( i > 1 && line.chars().nth(i - 2).unwrap() == charac);
+        let cur_key: String = vec![line.chars().nth(i-1).unwrap(), charac].into_iter().collect();
+        if couple_letter.contains_key(&cur_key){ 
+            good = good || couple_letter[&cur_key] + 1 != i;
+        } else {
+            couple_letter.insert(cur_key, i); 
+        }
+    }
+    return if double && good {1} else {0};
+}
+
 fn main() {
     let (content, mode) = init();
     let lines = content.split("\n");
     let mut result = 0;
     for line in lines{
-        result += analyze_word(line);
+        if mode.eq("1"){
+            result += analyze_word(line);
+        } else{
+            result += analyze_word_part_2(line);
+        }
     }
     println!("{}", result);
 }
